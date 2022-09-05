e382ab32dcece86763abd462cf574cb5b68954b4
Adil Hasan <adilhasan927@gmai.com>
2022-09-05 12:21:45 +0800

    mergeinsert working

--- SC2001 Project 1.c
+++ SC2001 Project 1.c
@@ -7,30 +7,12 @@
 
 int CMP_CNT;
 
-typedef void* (SORT_FN_PTR)(int, int, int*);
+typedef void* (SORT_FN_PTR)(int, int, int, int*);
 
 typedef struct timing_info {
 	double cpu_time;
 	int cmp_cnt;
 } TIMING_INFO;
-
-void InsertionSort(int* arr_start, int n);
-
-int test_alg_on_rand(int count, SORT_FN_PTR fn_ptr, TIMING_INFO* timing_info);
-
-int* gen_input_data(int count);
-
-int test_is_sorted(int* array, int count);
-
-int main()
-{
-	srand(time(NULL));   // Initialization, should only be called once
-
-	TIMING_INFO timing_info;
-	int success = test_alg_on_rand(10, &InsertionSort, &timing_info);
-
-	printf("test success? (0|1): %d", success);
-}
 
 int cmp_lt(int a, int b) {
 	CMP_CNT++;
@@ -52,6 +34,26 @@ int cmp_gte(int a, int b) {
 	return a >= b;
 }
 
+void InsertionSort(int* arr_start, int n);
+
+int test_alg_on_rand(int count, SORT_FN_PTR fn_ptr, TIMING_INFO* timing_info);
+
+int* gen_input_data(int count);
+
+void MergeInsert(int first, int last, int S, int* arr_start);
+
+int test_is_sorted(int* array, int count);
+
+int main()
+{
+	srand(time(NULL));   // Initialization, should only be called once
+
+	TIMING_INFO timing_info;
+	int success = test_alg_on_rand(10, &MergeInsert, 5, &timing_info);
+
+	printf("test success? (0|1): %d", success);
+}
+
 void swap(int* arr, int a, int b)
 {
 	int temp = arr[b];
@@ -59,7 +61,9 @@ void swap(int* arr, int a, int b)
 	arr[a] = temp;
 }
 
-void InsertionSort(int* arr_start, int n) {
+void InsertionSort(int start_idx, int end_idx, int unused, int* arr) {
+	int* arr_start = &(arr[start_idx]);
+	int n = (end_idx - start_idx) + 1;
 	for (int i = 1; i < n; i++) {
 		for (int j = i; j > 0; j--) {
 			if (cmp_lt(arr_start[j], arr_start[j - 1]))
@@ -69,7 +73,67 @@ void InsertionSort(int* arr_start, int n) {
 	}
 }
 
-int test_alg_on_rand(int count, SORT_FN_PTR fn_ptr, TIMING_INFO* timing_info) {
+void merge(int start, int end, int* arr_start) {
+	int mid = (start + end) / 2;
+
+	int first_bucket = start;
+	int second_bucket = mid + 1;
+	
+	int i;
+	int tmp;
+	int fst_bucket_gt_snd_bucket;
+
+	if (end - start <= 0) {
+		return;
+	}
+
+	while (first_bucket <= mid && second_bucket <= end) {
+		fst_bucket_gt_snd_bucket = cmp_gt(arr_start[first_bucket], arr_start[second_bucket]);
+
+		if (fst_bucket_gt_snd_bucket) {
+			//set tmp to the first element in the second bucket
+			tmp = arr_start[second_bucket++];
+
+			for (i = ++mid; i > first_bucket; i--) {
+				arr_start[i] = arr_start[i - 1];
+			}
+			arr_start[first_bucket++] = tmp;
+		}
+
+		else if (!fst_bucket_gt_snd_bucket) {
+			first_bucket++;
+		}
+
+		else {
+			if (first_bucket == mid && second_bucket == end) {
+				break;
+			}
+			tmp = arr_start[second_bucket++];
+			first_bucket++;
+			for (i = ++mid; i > first_bucket; i--) {
+				arr_start[i] = arr_start[i - 1];
+			}
+			arr_start[first_bucket++] = tmp;
+		}
+	}
+}
+
+void MergeInsert(int first, int last, int S, int* arr_start) { // Hybrid Algorithm
+	if (last - first <= 0) {
+		return;
+	}
+	else if (last - first > S) {
+		int mid = (first + last) / 2;
+		MergeInsert(first, mid, S, arr_start);
+		MergeInsert(mid + 1, last, S, arr_start);
+		merge(first, last, arr_start);
+	}
+	else if (last - first <= S) {
+		InsertionSort(first, last, 0, arr_start);
+	}
+}
+
+int test_alg_on_rand(int count, SORT_FN_PTR fn_ptr, int size_for_mergeinsert, TIMING_INFO* timing_info) {
 	CMP_CNT = 0;
 
 	clock_t start, end;
@@ -77,9 +141,9 @@ int test_alg_on_rand(int count, SORT_FN_PTR fn_ptr, TIMING_INFO* timing_info) {
 
 	start = clock();
 
-	int array = gen_input_data(count);
+	int* array = gen_input_data(count);
 
-	fn_ptr(0, count - 1, array);
+	fn_ptr(0, count - 1, size_for_mergeinsert, array);
 
 	if (!test_is_sorted(array, count)) {
 		return 0;
